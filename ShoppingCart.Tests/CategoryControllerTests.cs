4e860e0b87c74a7c533966e417eee84e97d976ef
andriiguz <andrii.huz@nure.ua>
2024-05-22 20:31:51 +0300

    commit for Lab3

---
+++ ShoppingCart.Tests/CategoryControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Moq;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+using System.Linq;
+using Xunit;
+using ShoppingCart.Tests.Datasets;
+
+namespace ShoppingCart.Tests
+{
+    public class CategoryControllerTests
+    {
+        private Mock<IUnitOfWork> mockUnitOfWork;
+        private Mock<ICategoryRepository> repositoryMock;
+        private ShoppingCart.Web.Areas.Admin.Controllers.CategoryController controller;
+
+        public CategoryControllerTests()
+        {
+            repositoryMock = new Mock<ICategoryRepository>();
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+            controller = new ShoppingCart.Web.Areas.Admin.Controllers.CategoryController(mockUnitOfWork.Object);
+        }
+
+        [Fact]
+        public void CreateUpdate_WithNewCategory_CreatesCategory()
+        {
+            var categoryVM = new CategoryVM { Category = new Category { Id = 0, Name = "TestCategory" } };
+            controller.CreateUpdate(categoryVM);
+            repositoryMock.Verify(r => r.Add(It.IsAny<Category>()), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [Fact]
+        public void CreateUpdate_WithExistingCategory_UpdatesCategory()
+        {
+            var categoryVM = new CategoryVM { Category = new Category { Id = 1, Name = "TestCategory" } };
+            controller.CreateUpdate(categoryVM);
+            repositoryMock.Verify(r => r.Update(It.IsAny<Category>()), Times.Once);
+            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteData_WithNonExistingId_ThrowsException()
+        {
+            int nonExistingCategoryId = 999;
+            Assert.Throws<Exception>(() => controller.DeleteData(nonExistingCategoryId));
+        }
+    }
+}
---
+++ ShoppingCart.Tests/OrderControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using Moq;
+using ShoppingCart.DataAccess.Repositories;
+using ShoppingCart.DataAccess.ViewModels;
+using ShoppingCart.Models;
+using ShoppingCart.Utility;
+using ShoppingCart.Web.Areas.Admin.Controllers;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace ShoppingCart.Tests.Controllers
+{
+    public class OrderControllerTests
+    {
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private OrderController _controller;
+
+        public OrderControllerTests()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _controller = new OrderController(_mockUnitOfWork.Object);
+        }
+
+        [Fact]
+        public void SetToInProcess_UpdatesOrderStatus()
+        {
+            var orderVM = new OrderVM
+            {
+                OrderHeader = new OrderHeader { Id = 1 }
+            };
+
+            _mockUnitOfWork.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Expression<Func<OrderHeader, bool>>>(), null)).Returns(orderVM.OrderHeader);
+
+            _controller.SetToInProcess(orderVM);
+            _mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [Fact]
+        public void SetToShipped_UpdatesOrderHeader()
+        {
+            var orderVM = new OrderVM
+            {
+                OrderHeader = new OrderHeader
+                {
+                    Id = 1,
+                    Carrier = "TprzTestlab3",
+                    TrackingNumber = "22052024"
+                }
+            };
+            var orderHeader = new OrderHeader { Id = 1 };
+
+            _mockUnitOfWork.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Expression<Func<OrderHeader, bool>>>(), null))
+                           .Returns(orderHeader);
+
+            _controller.SetToShipped(orderVM);
+
+            Assert.Equal(orderVM.OrderHeader.Carrier, orderHeader.Carrier);
+            Assert.Equal(orderVM.OrderHeader.TrackingNumber, orderHeader.TrackingNumber);
+            Assert.Equal(OrderStatus.StatusShipped, orderHeader.OrderStatus);
+            Assert.NotNull(orderHeader.DateOfShipping);
+
+            _mockUnitOfWork.Verify(uow => uow.OrderHeader.Update(orderHeader), Times.Once);
+            _mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+
+        [Fact]
+        public void SetToCancelOrder_ApprovesRefundIfPaid()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void SetToCancelOrder_CancelsIfNotPaid()
+        {
+            var orderVM = new OrderVM
+            {
+                OrderHeader = new OrderHeader
+                {
+                    Id = 1,
+                    PaymentStatus = PaymentStatus.StatusPending
+                }
+            };
+            var orderHeader = new OrderHeader { Id = 1, PaymentStatus = PaymentStatus.StatusPending };
+
+            _mockUnitOfWork.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Expression<Func<OrderHeader, bool>>>(), null))
+                           .Returns(orderHeader);
+
+            _controller.SetToCancelOrder(orderVM);
+            _mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+        }
+    }
+}
